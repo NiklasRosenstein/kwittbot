1ccf682afee8b03b5bdebd772b133bc648d0be68
Niklas Rosenstein <rosensteinniklas@gmail.com>
2017-06-25 10:44:53 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+
+import logging
+import signal
+from telegram.ext import Updater, MessageHandler, InlineQueryHandler, Filters
+from telegram import InputTextMessageContent, InlineQueryResultArticle
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton
+
+def message(bot, update):
+  pass
+
+
+def inline(bot, update):
+  query = update.inline_query
+
+  markup = InlineKeyboardMarkup([
+    [InlineKeyboardButton("Send Money", callback_data='send')]
+  ])
+  content = InputTextMessageContent('Uh, how much money exactly?')
+  article = InlineQueryResultArticle('SomeID', 'Send Money', content, reply_markup=markup)
+
+  query.answer([article])
+
+
+def error(bot, update, error):
+  logging.warning('Update "%s" caused error "%s"' % (update, error))
+
+
+def main():
+  logging.basicConfig(format='[%(levelname)s - %(asctime)s]: %(message)s', level=logging.INFO)
+  logging.info('Firing up KwittBot ...')
+
+  updater = Updater('383406996:AAF08E4KvFmu2yNjp2EAqWCzKYeupw5nT90')
+  updater.dispatcher.add_handler(MessageHandler(Filters.text, message))
+  updater.dispatcher.add_handler(InlineQueryHandler(inline))
+  updater.dispatcher.add_error_handler(error)
+  updater.start_polling()
+
+  logging.info('Polling started, entering IDLE ...')
+  updater.idle()
+
+
+if require.main == module:
+  main()
